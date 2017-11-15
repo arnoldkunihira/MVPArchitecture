71410b147b84408fa7faca768c9e365180b31fa8
arnold5sweet <arnold.kmulera@gmail.com>
2017-11-15 17:19:46 +0300

    MVP Implementation

---
+++ app/src/main/java/com/mindorks/mvp/ui/login/LoginPresenter.java
@@ -0,0 +1,39 @@
+/*
+ *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.mindorks.mvp.ui.login;
+
+import com.mindorks.mvp.data.DataManager;
+import com.mindorks.mvp.ui.base.BasePresenter;
+
+/**
+ * Created by Arnold on 22-10-2017.
+ */
+
+public class LoginPresenter<V extends LoginMvpView> extends BasePresenter<V> implements LoginMvpPresenter<V> {
+
+    public LoginPresenter(DataManager dataManager) {
+        super(dataManager);
+    }
+
+    @Override
+    public void startLogin(String emailId) {
+        getDataManager().saveEmailId(emailId);
+        getDataManager().setLoggedIn();
+        getMvpView().openMainActivity();
+    }
+
+}
